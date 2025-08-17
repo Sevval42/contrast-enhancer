a597784041f20e608b0226fd12c4b9abb69a7e11
Seval Y <sevval.55@outlook.de>
2025-08-17 13:25:50 +0200

    first histogram initializer test

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,16 @@
 VulkanContext* context;
 VulkanDescriptorSet* descriptorSetInfo;
 VulkanPipeline pipeline;
-VulkanBuffer ioBuffer;
-VulkanBuffer firstTempBuffer;
+
+VulkanBuffer uniformsBuffer;
 VulkanImage imageBuffer;
 size_t imageSize;
-float myData[] = {1, 2, 3, 4, 5};
+VulkanBuffer histogramBuffer;
 
 struct UniformData {
     float offset = 4;
+    uint32_t kernelRadius = 3;
+    float defaultColor = 1;
 }uniformData;
 
 void initApplication() {
@@ -54,37 +56,27 @@ void initApplication() {
     LOG("Creating descriptor set");
     descriptorSetInfo = initDescriptorSet();
 
-    addDescriptorSetLayout(descriptorSetInfo, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
     addDescriptorSetLayout(descriptorSetInfo, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
     addDescriptorSetLayout(descriptorSetInfo, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE);
+    addDescriptorSetLayout(descriptorSetInfo, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
     createDescriptorSet(context, descriptorSetInfo);
 
 
     LOG("Filling descriptorsets with Buffers");
-    descriptorSetInfo->addBufferAndData(
-        context, 
-        &ioBuffer, 
-        myData,
-        sizeof(myData), 
-        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT , 
-        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
-    );
-
-    descriptorSetInfo->addBufferAndData(
-        context,
-        &firstTempBuffer,
-        &uniformData,
-        sizeof(uniformData),
-        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+    descriptorSetInfo->addBufferAndData(context, 
+        &uniformsBuffer, 
+        &uniformData, 
+        sizeof(uniformData), 
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, 
         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
     );
 
     int w,h,channels;
-    unsigned char* pixels = stbi_load("../images/image.png", &w, &h, &channels, STBI_rgb_alpha);
+    unsigned char* pixels = stbi_load("../images/image3.png", &w, &h, &channels, STBI_rgb_alpha);
     if (!pixels) {
         throw std::runtime_error("Failed to load image");
     }
-    imageSize = size_t(w) * h * channels;
+    imageSize = size_t(w) * h * 4;
 
     descriptorSetInfo->addImageAndData(
         context, 
@@ -96,17 +88,21 @@ void initApplication() {
     );
     stbi_image_free(pixels);
 
+    descriptorSetInfo->addBufferAndData(context, 
+        &histogramBuffer, 
+        NULL, 
+        sizeof(uint) * 256 * 256 * 256, 
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+    );
+
     LOG("Load descriptor set");
     fillDescriptorSet(context, descriptorSetInfo);
 
     LOG("Creating pipeline");
     std::vector<const char*> computeShaders;
     computeShaders.push_back("../shaders/test1.spv");
-    computeShaders.push_back("../shaders/test2.spv");
-    computeShaders.push_back("../shaders/test3.spv");
     std::vector<ivec3> dispatches = {
-        ivec3{5, 1, 1},
-        ivec3{1, 1, 1},
         ivec3{(int)w/16+1, (int)h/16+1, 1},
     };
     pipeline = createPipeline(context, computeShaders, dispatches, descriptorSetInfo);
@@ -115,9 +111,9 @@ void initApplication() {
 void shutdownApplication() {
     vkDeviceWaitIdle(context->device);
     
+    destroyBuffer(context, &histogramBuffer);
     destroyImage(context, &imageBuffer);
-    destroyBuffer(context, &firstTempBuffer);
-    destroyBuffer(context, &ioBuffer);
+    destroyBuffer(context, &uniformsBuffer);
     destroyPipeline(context, &pipeline);
     destroyDescriptorSet(context, descriptorSetInfo);
     
@@ -190,15 +186,6 @@ void runApplication() {
 
     vkQueueWaitIdle(context->computeQueue.queue);
 
-    float data[5];
-    getDataFromBufferWithStagingBuffer(context, &ioBuffer, data, sizeof(myData));
-
-    std::cout << "[" << data[0];
-    for (int i = 1; i < 5; i++) {
-        std::cout<< ", " << data[i];
-    }
-    std::cout << "]" << std::endl;
-
     vkFreeCommandBuffers(context->device, context->commandPool, 1, &commandBuffer);
 }
 
@@ -216,6 +203,40 @@ int main(int argc, char* argv[]) {
         LOG_ERROR("Failed saving output image");
     }
 
+    
+    size_t count = 256*256*256;
+    std::vector<uint> histogram(count);
+    getDataFromBufferWithStagingBuffer(context, &histogramBuffer, histogram.data(), sizeof(int) * count);
+
+    std::vector<uint> histogram2D(256*256);
+
+    for(uint i = 0; i < 256*256; ++i) {
+        int x = i % 256;
+        int y = floor(i/256);
+        for(int z = 0; z < 256; ++z) {
+            int getIndex = x + z * 256 + y * 256 * 256;
+            histogram2D[i] += histogram[getIndex];
+        }
+    }
+
+    uint max = 0;
+    for(uint i = 0; i < 256*256; ++i) {
+        if(histogram2D[i]>max) {
+            max = histogram2D[i];
+        }
+    }
+
+    for(uint i = 0; i < 256*256; ++i) {
+        histogram2D[i] = (int)((float)histogram2D[i] / (float) max * 255);
+    }
+
+    std::vector<uint8_t> histogram2DBytes(256*256);
+    for (size_t i = 0; i < 256*256; ++i) {
+        histogram2DBytes[i] = std::min(histogram2D[i], 255u);
+    }
+    stbi_write_png("output.png", 256, 256, 1, histogram2DBytes.data(), 256);
+
+   
     shutdownApplication();
     return 1;
 }
