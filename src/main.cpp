086ab7d9bb6a31214acd3314a765c9a0f655103b
Seval Y <sevval.55@outlook.de>
2025-08-17 19:34:19 +0200

    fix small buffer size error

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,7 @@ void initApplication(std::string imageFile) {
     descriptorSetInfo->addBufferAndData(context, 
         &expLookUp, 
         lookUp, 
-        sizeof(float) * constants.kernelRadius*2+1, 
+        sizeof(float) * (constants.kernelRadius*2+1), 
         VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 
         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
     );
